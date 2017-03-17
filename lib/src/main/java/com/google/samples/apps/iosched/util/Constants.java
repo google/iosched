8e16c6f584be59db57c3a0a74d7bd3c099999598
Shailen Tuli <shailentuli@google.com>
2017-03-17 07:50:45 -0700

    Added Google sign in as part of onboarding flow.
    Change-Id: I6b250b02ea9f4d89e5ac2789902271745baf24df

---
+++ lib/src/main/java/com/google/samples/apps/iosched/util/Constants.java
@@ -0,0 +1,12 @@
+package com.google.samples.apps.iosched.util;
+
+/**
+ * Constants used by util files.
+ */
+public class Constants {
+    /**
+     * This is changed each year to effectively reset certain preferences that should be re-asked
+     * each year. Note, res/xml/settings_prefs.xml must be updated when this value is updated.
+     */
+    public static final String CONFERENCE_YEAR_PREF_POSTFIX = "_2017";
+}
