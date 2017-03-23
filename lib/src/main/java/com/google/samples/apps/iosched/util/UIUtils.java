5978bf69976a0076c2050a16f926209eee7d2588
Jonathan Koren <jdkoren@google.com>
2017-03-23 13:30:54 -0700

    Remove DrawShadowFrameLayout
    It basically does nothing on API 21+.
    Bug: 36071900
    
    Change-Id: I86a755d7d0317347bfec35bf6ec9b62e53df5ad1

--- lib/src/main/java/com/google/samples/apps/iosched/util/UIUtils.java
+++ lib/src/main/java/com/google/samples/apps/iosched/util/UIUtils.java
@@ -24,8 +24,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
-import android.content.res.Resources;
-import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -328,29 +326,6 @@ public class UIUtils {
     }
 
     private static final int[] RES_IDS_ACTION_BAR_SIZE = {R.attr.actionBarSize};
-
-    /**
-     * Calculates the Action Bar height in pixels.
-     */
-    public static int calculateActionBarSize(Context context) {
-        if (context == null) {
-            return 0;
-        }
-
-        Resources.Theme curTheme = context.getTheme();
-        if (curTheme == null) {
-            return 0;
-        }
-
-        TypedArray att = curTheme.obtainStyledAttributes(RES_IDS_ACTION_BAR_SIZE);
-        if (att == null) {
-            return 0;
-        }
-
-        float size = att.getDimension(0, 0);
-        att.recycle();
-        return (int) size;
-    }
 
     public static int setColorOpaque(int color) {
         return Color.argb(255, Color.red(color), Color.green(color), Color.blue(color));
