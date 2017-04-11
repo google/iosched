d4a472bd499c2d5bda4b7bab85f856983a7d8b9f
Nick Butcher <nickbutcher@google.com>
2017-04-11 11:33:40 +0100

    More styling tweaks to Info screens.
    Bug: 36797124
    Change-Id: I435492131ee8d0c90e62a77a633b43cdd0a71055

---
+++ lib/src/main/java/com/google/samples/apps/iosched/info/event/EventView.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.samples.apps.iosched.info.event;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.samples.apps.iosched.lib.R;
+
+public class EventView extends FrameLayout {
+
+    private TextView mDescriptionView;
+
+    public EventView(Context context) {
+        this(context, null);
+    }
+
+    public EventView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public EventView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        TypedArray arr = context.obtainStyledAttributes(attrs, R.styleable.EventView, 0, 0);
+        String eventTitle = arr.getString(R.styleable.EventView_eventTitle);
+        String eventDescription = arr.getString(R.styleable.EventView_eventDescription);
+        Drawable eventIconDrawable = arr.getDrawable(R.styleable.EventView_eventIcon);
+        int boxColor = arr.getColor(R.styleable.EventView_boxColor,
+                ContextCompat.getColor(getContext(), R.color.sunflower_yellow));
+        arr.recycle();
+
+        View rootView = LayoutInflater.from(context)
+                .inflate(R.layout.info_event_content_card_view, this, true);
+        TextView titleView = (TextView) rootView.findViewById(R.id.event_title);
+        mDescriptionView = (TextView) rootView.findViewById(R.id.event_content_description);
+        mDescriptionView.setText(eventDescription);
+        View header = rootView.findViewById(R.id.header);
+        ImageView iconView = (ImageView) rootView.findViewById(R.id.event_icon);
+        titleView.setText(eventTitle);
+        header.setBackgroundColor(boxColor);
+        iconView.setImageDrawable(eventIconDrawable);
+        rootView.findViewById(R.id.event_view_sessions).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO link to sessions
+            }
+        });
+        rootView.findViewById(R.id.event_view_map).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO link to map
+            }
+        });
+    }
+
+    public void setEventDescription(CharSequence description) {
+        mDescriptionView.setText(description);
+    }
+}
