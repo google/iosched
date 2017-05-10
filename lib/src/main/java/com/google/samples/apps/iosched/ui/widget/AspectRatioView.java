f3e3c89deb65408eec4df2b5b285cc375f6c0b28
Nick Butcher <nickbutcher@google.com>
2017-05-10 12:37:42 +0100

    Improvements to session details livestream controls.
    - Background protection for contrast over light backgrounds.
    - Fixed icon tints
    - Capped header height on short/landscape devices.
    
    Bug: 38187058
    Change-Id: Ie24efd1d0ac0b2c97612d42e04565beca8917a60

--- lib/src/main/java/com/google/samples/apps/iosched/ui/widget/AspectRatioView.java
+++ lib/src/main/java/com/google/samples/apps/iosched/ui/widget/AspectRatioView.java
@@ -29,7 +29,10 @@ import com.google.samples.apps.iosched.lib.R;
  */
 public class AspectRatioView extends FrameLayout {
 
+    private static final int NO_MAX_HEIGHT = -1;
+
     private float mAspectRatio = 0f;
+    private int mMaxHeight = NO_MAX_HEIGHT;
 
     public AspectRatioView(Context context) {
         this(context, null, 0);
@@ -45,19 +48,22 @@ public class AspectRatioView extends FrameLayout {
         final TypedArray a = context.obtainStyledAttributes(attrs,
                 R.styleable.AspectRatioView, defStyle, 0);
         mAspectRatio = a.getFloat(R.styleable.AspectRatioView_aspectRatio, 0);
-
         if (mAspectRatio == 0f) {
             throw new IllegalArgumentException(
                     "You must specify an aspect ratio when using AspectRatioView.");
         }
+        mMaxHeight = a.getDimensionPixelSize(R.styleable.AspectRatioView_maxHeight, NO_MAX_HEIGHT);
         a.recycle();
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         if (mAspectRatio != 0) {
-            final int width = MeasureSpec.getSize(widthMeasureSpec);
-            final int height = (int) (width / mAspectRatio);
+            int width = MeasureSpec.getSize(widthMeasureSpec);
+            int height = (int) (width / mAspectRatio);
+            if (mMaxHeight > 0) {
+                height = Math.min(height, mMaxHeight);
+            }
             super.onMeasure(
                     MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY),
                     MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY));
