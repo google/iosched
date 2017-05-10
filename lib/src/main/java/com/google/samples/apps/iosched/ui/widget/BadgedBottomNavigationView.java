6795c45ccc8a9acd249fa04c1eab53034ace9996
Nick Butcher <nickbutcher@google.com>
2017-05-10 16:43:32 +0100

    Animate Feed indicator badge on tab change.
    Bug|: 38195231
    
    Change-Id: I4c804255ffa1cd84a96549a1cd020bb6027031c3

--- lib/src/main/java/com/google/samples/apps/iosched/ui/widget/BadgedBottomNavigationView.java
+++ lib/src/main/java/com/google/samples/apps/iosched/ui/widget/BadgedBottomNavigationView.java
@@ -22,10 +22,14 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.IntRange;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.util.AttributeSet;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 
 import com.google.samples.apps.iosched.lib.R;
 
@@ -40,6 +44,15 @@ public class BadgedBottomNavigationView extends BottomNavigationView {
     private int badgePosition = NO_BADGE_POSITION;
     private Paint badgePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private float radius;
+    private @Nullable OnNavigationItemSelectedListener listener;
+    private ViewTreeObserver.OnDrawListener drawListener = new ViewTreeObserver.OnDrawListener() {
+        @Override
+        public void onDraw() {
+            if (badgePosition > NO_BADGE_POSITION) {
+                postInvalidateOnAnimation();
+            }
+        }
+    };
 
     public BadgedBottomNavigationView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -49,6 +62,20 @@ public class BadgedBottomNavigationView extends BottomNavigationView {
                 a.getColor(R.styleable.BadgedBottomNavigationView_badgeColor, Color.TRANSPARENT));
         radius = a.getDimension(R.styleable.BadgedBottomNavigationView_badgeRadius, 0f);
         a.recycle();
+
+        // we need to listen to tab selection to re-position the badge so set our own listener
+        // wrapping any provided listener.
+        super.setOnNavigationItemSelectedListener(new OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (badgePosition > NO_BADGE_POSITION) {
+                    // use a pre-draw listener to invalidate ourselves when the badged item moves
+                    getViewTreeObserver().addOnDrawListener(drawListener);
+                }
+                if (listener != null) return listener.onNavigationItemSelected(item);
+                return false;
+            }
+        });
     }
 
     public void showBadge(@IntRange(from = 0) int itemPosition) {
@@ -75,5 +102,17 @@ public class BadgedBottomNavigationView extends BottomNavigationView {
             float cx = menuView.getLeft() + menuItem.getRight() - icon.getLeft();
             canvas.drawCircle(cx, icon.getTop(), radius, badgePaint);
         }
+    }
+
+    @Override
+    public void setOnNavigationItemSelectedListener(
+            @Nullable OnNavigationItemSelectedListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        getViewTreeObserver().removeOnDrawListener(drawListener);
+        super.onDetachedFromWindow();
     }
 }
