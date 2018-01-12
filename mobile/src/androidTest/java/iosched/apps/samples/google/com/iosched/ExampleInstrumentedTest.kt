66fde08d7d93827a5f11d50a2bcad0aabb10ac0b
shailen <shailen.tuli@gmail.com>
2018-01-11 19:35:55 -0800

    Deleted all old IOSched code.
    BUG: 71877447
    
    Replaced with a (mostly) empty shell project.
    
    Note that the new main module is now called mobile, which is the name used
    by Studio when creating a mobile, wear, and ATV project.
    
    Note, too, that playstore/ is left untouched.
    
    Change-Id: I1e9ea9acefa4a409eb0f0de2f5d86455142f8d56

---
+++ mobile/src/androidTest/java/iosched/apps/samples/google/com/iosched/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package iosched.apps.samples.google.com.iosched
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("iosched.apps.samples.google.com.iosched", appContext.packageName)
+    }
+}
--- third_party/material-components-android/lib/src/android/support/design/internal/package-info.java
+++
@@ -1,7 +0,0 @@
-/** @hide */
-@RestrictTo(LIBRARY_GROUP)
-package android.support.design.internal;
-
-import static android.support.annotation.RestrictTo.Scope.LIBRARY_GROUP;
-
-import android.support.annotation.RestrictTo;
