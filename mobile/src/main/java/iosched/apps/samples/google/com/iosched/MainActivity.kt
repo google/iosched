4c311a0d159ae77de7263098447ffab717cb674a
shailen <shailen.tuli@gmail.com>
2018-01-11 19:35:55 -0800

    Deleted all old IOSched code.
    BUG: 71877447
    
    Replaced with a (mostly) empty shell project.
    
    Note that the new main module is now called mobile, which is the name used
    by Studio when creating a mobile, wear, and ATV project.
    
    Note, too, that playstore/ is left untouched.
    
    Change-Id: I1e9ea9acefa4a409eb0f0de2f5d86455142f8d56

---
+++ mobile/src/main/java/iosched/apps/samples/google/com/iosched/MainActivity.kt
@@ -0,0 +1,12 @@
+package iosched.apps.samples.google.com.iosched
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
