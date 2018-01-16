41f050528b0e143f3342a7f7989fed8fb7821e21
shailen <shailen.tuli@gmail.com>
2018-01-16 12:00:55 -0800

    Created models that define the data logic for the app.
    This is a first pass, and the models are likely to be refined in subsequent
    commits.
    
    Note: this commit does not cover the models required to render the map or the
    feed, or to define user data.
    
    BUG: 72047935
    Change-Id: I507436737696bc189c46262b8c66ce274a263d30

---
+++ shared/src/test/java/iosched/apps/samples/google/com/shared/ExampleUnitTest.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2018 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package iosched.apps.samples.google.com.shared
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
