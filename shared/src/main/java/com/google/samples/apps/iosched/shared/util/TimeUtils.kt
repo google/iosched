05b404ba4110bbb07251d42e9b053334f795490f
Jonathan Koren <jdkoren@FIXME>
2018-01-19 16:20:33 -0800

    Add conference dates & times via Gradle
    Change-Id: Ib6b2a725f5bdc97cf648a37748e3d4b4341c251a

---
+++ shared/src/main/java/com/google/samples/apps/iosched/shared/util/TimeUtils.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.iosched.shared.util
+
+import com.google.samples.apps.iosched.shared.BuildConfig
+import com.google.samples.apps.iosched.shared.model.Session
+import org.threeten.bp.ZoneId
+import org.threeten.bp.ZonedDateTime
+
+object TimeUtils {
+    val CONFERENCE_TIMEZONE = ZoneId.of(BuildConfig.CONFERENCE_TIMEZONE)
+    val CONFERENCE_DAYS = ConferenceDay.values()
+
+    enum class ConferenceDay(val start: ZonedDateTime, val end: ZonedDateTime) {
+        PRECONFERENCE_DAY(ZonedDateTime.parse(BuildConfig.PRECONFERENCE_DAY_START),
+                ZonedDateTime.parse(BuildConfig.PRECONFERENCE_DAY_END)),
+        DAY_1(ZonedDateTime.parse(BuildConfig.CONFERENCE_DAY1_START),
+                ZonedDateTime.parse(BuildConfig.CONFERENCE_DAY1_END)),
+        DAY_2(ZonedDateTime.parse(BuildConfig.CONFERENCE_DAY2_START),
+                ZonedDateTime.parse(BuildConfig.CONFERENCE_DAY2_END)),
+        DAY_3(ZonedDateTime.parse(BuildConfig.CONFERENCE_DAY3_START),
+                ZonedDateTime.parse(BuildConfig.CONFERENCE_DAY3_END));
+
+        fun contains(session: Session) = start <= session.startTime && end >= session.endTime
+    }
+}
