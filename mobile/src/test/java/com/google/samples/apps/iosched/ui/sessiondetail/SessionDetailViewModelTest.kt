57f46231488500890dbd5fcf48c1a02234409177
Sean McQuillan <seanmcq@google.com>
2018-03-22 13:38:08 -0700

    Add session start countdown timer to SessionDetail
    Changes:
     * Added SetIntervalLiveData for mapped liveData that have a time-dependency
     * Added DefaultTime which provides MockableTime
     * Use SetIntervalLiveData to show a countdown view on SessionDetail.
    
    Bug: 75407081
    Change-Id: Ia66697005b6d5d6f34ac133f07158b4e77ab331b

--- mobile/src/test/java/com/google/samples/apps/iosched/ui/sessiondetail/SessionDetailViewModelTest.kt
+++ mobile/src/test/java/com/google/samples/apps/iosched/ui/sessiondetail/SessionDetailViewModelTest.kt
@@ -27,6 +27,8 @@ import com.google.samples.apps.iosched.shared.model.Session
 import com.google.samples.apps.iosched.shared.util.TimeUtils
 import com.google.samples.apps.iosched.test.util.LiveDataTestUtil
 import com.google.samples.apps.iosched.test.util.SyncTaskExecutorRule
+import com.google.samples.apps.iosched.test.util.time.FixedTimeExecutorRule
+import com.google.samples.apps.iosched.util.SetIntervalLiveData
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertNull
@@ -34,6 +36,7 @@ import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.threeten.bp.Duration
 
 /**
  * Unit tests for the [SessionDetailViewModel].
@@ -46,32 +49,41 @@ class SessionDetailViewModelTest {
     // Executes tasks in a synchronous [TaskScheduler]
     @get:Rule var syncTaskExecutorRule = SyncTaskExecutorRule()
 
+    // Allows explicit setting of "now"
+    @get:Rule var fixedTimeExecutorRule = FixedTimeExecutorRule()
+
     private lateinit var viewModel: SessionDetailViewModel
     private val testSession = TestData.session0
 
-    @Before fun setup() {
+    @Before
+    fun setup() {
         viewModel = SessionDetailViewModel(createUseCase(testSession))
         viewModel.loadSessionById(testSession.id)
     }
 
-    @Test fun testDataIsLoaded_observablesUpdated() {
+    @Test
+    fun testDataIsLoaded_observablesUpdated() {
         assertEquals(testSession, LiveDataTestUtil.getValue(viewModel.session))
     }
 
-    @Test fun testCheckPlayable_currentSessionNull() {
+    @Test
+    fun testCheckPlayable_currentSessionNull() {
         assertFalse(viewModel.checkPlayable(null))
     }
 
-    @Test fun testCheckPlayable_currentSessionBlankUrl() {
+    @Test
+    fun testCheckPlayable_currentSessionBlankUrl() {
         assertFalse(viewModel.checkPlayable(createSessionWithUrl("  ")))
     }
 
-    @Test fun testCheckPlayable_currentSessionHasUrl() {
+    @Test
+    fun testCheckPlayable_currentSessionHasUrl() {
         val rickSession = createSessionWithUrl("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
         assertTrue(viewModel.checkPlayable(rickSession))
     }
 
-    @Test fun testOnPlayVideo_createsEventForVideo() {
+    @Test
+    fun testOnPlayVideo_createsEventForVideo() {
         val sessionWithUrl = createSessionWithUrl("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
         val vm = SessionDetailViewModel(createUseCase(sessionWithUrl))
 
@@ -80,11 +92,49 @@ class SessionDetailViewModelTest {
         LiveDataTestUtil.getValue(vm.session)
 
         vm.onPlayVideo()
-        assertEquals(sessionWithUrl.youTubeUrl,
-                LiveDataTestUtil.getValue(vm.navigateToYouTubeAction)?.peekContent())
+        assertEquals(
+            sessionWithUrl.youTubeUrl,
+            LiveDataTestUtil.getValue(vm.navigateToYouTubeAction)?.peekContent()
+        )
     }
 
-    @Test fun testOnPlayVideo_doesNotcreateEventForVideo() {
+    @Test
+    fun testStartsInTenMinutes_thenHasNullTimeUntilStart() {
+        fixedTimeExecutorRule.time = testSession.startTime.minusMinutes(10).toInstant()
+        forceTimeUntilStartIntervalUpdate()
+        assertEquals(null, LiveDataTestUtil.getValue(viewModel.timeUntilStart))
+    }
+
+    @Test
+    fun testStartsIn5Minutes_thenHasDurationTimeUntilStart() {
+        fixedTimeExecutorRule.time = testSession.startTime.minusMinutes(5).toInstant()
+        forceTimeUntilStartIntervalUpdate()
+        assertEquals(Duration.ofMinutes(5), LiveDataTestUtil.getValue(viewModel.timeUntilStart))
+    }
+
+    @Test
+    fun testStartsIn1Minutes_thenHasDurationTimeUntilStart() {
+        fixedTimeExecutorRule.time = testSession.startTime.minusMinutes(1).toInstant()
+        forceTimeUntilStartIntervalUpdate()
+        assertEquals(Duration.ofMinutes(1), LiveDataTestUtil.getValue(viewModel.timeUntilStart))
+    }
+
+    @Test
+    fun testStartsIn0Minutes_thenHasNullTimeUntilStart() {
+        fixedTimeExecutorRule.time = testSession.startTime.minusSeconds(30).toInstant()
+        forceTimeUntilStartIntervalUpdate()
+        assertEquals(null, LiveDataTestUtil.getValue(viewModel.timeUntilStart))
+    }
+
+    @Test
+    fun testStarts10MinutesAgo_thenHasNullTimeUntilStart() {
+        fixedTimeExecutorRule.time = testSession.startTime.plusMinutes(10).toInstant()
+        forceTimeUntilStartIntervalUpdate()
+        assertEquals(null, LiveDataTestUtil.getValue(viewModel.timeUntilStart))
+    }
+
+    @Test
+    fun testOnPlayVideo_doesNotCreateEventForVideo() {
         val sessionWithUrl = createSessionWithUrl("")
         val vm = SessionDetailViewModel(createUseCase(sessionWithUrl))
 
@@ -105,12 +155,18 @@ class SessionDetailViewModelTest {
         }
     }
 
+    private fun forceTimeUntilStartIntervalUpdate() {
+        (viewModel.timeUntilStart as SetIntervalLiveData<*, *>).updateValue()
+    }
+
     private fun createSessionWithUrl(youtubeUrl: String) =
-            Session(id = "0", title = "Session 0", abstract = "",
-                    startTime = TimeUtils.ConferenceDay.DAY_1.start,
-                    endTime = TimeUtils.ConferenceDay.DAY_1.end, room = TestData.room,
-                    sessionUrl = "", liveStreamUrl = "", youTubeUrl = youtubeUrl, photoUrl = "",
-                    tags = listOf(TestData.androidTag, TestData.webTag),
-                    displayTags = listOf(TestData.androidTag, TestData.webTag),
-                    speakers = setOf(TestData.speaker), relatedSessions = emptySet())
+        Session(
+            id = "0", title = "Session 0", abstract = "",
+            startTime = TimeUtils.ConferenceDay.DAY_1.start,
+            endTime = TimeUtils.ConferenceDay.DAY_1.end, room = TestData.room,
+            sessionUrl = "", liveStreamUrl = "", youTubeUrl = youtubeUrl, photoUrl = "",
+            tags = listOf(TestData.androidTag, TestData.webTag),
+            displayTags = listOf(TestData.androidTag, TestData.webTag),
+            speakers = setOf(TestData.speaker), relatedSessions = emptySet()
+        )
 }
