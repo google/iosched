df248023c851b92950028236436de8bc27632662
Doug Sigelbaum <sigelbaum@google.com>
2018-04-19 00:08:38 -0700

    Fix crashlytics.
    Each module has to now get its own "CrashlyticsTree" implementation.
    This is because if :shared implements CrashlyticsTree, then then
    :shared has to depend on the crashlytics SDK and needs the fabric
    plugin applied. When I do this, and point MainApplication,
    TvApplication, and WearApplication to :shared's CrashlyticsTree,
    I get cryptic compilation errors.
    
    If I instead do what we did with the other Firebase libs; make the
    SDK transitive in :shared (api instead of implementation) and remove
    the dependency from the application modules; then it silently fails.
    That's what we had before this CL.
    
    Therefore, only the application modules can have the crashlytics
    SDK dependency + fabric plugin applied.
    
    Bug: 78259510
    
    Change-Id: I92d0769ebd0d15cf2230e02ad70df3c554c9a40e

--- wear/src/main/java/com/google/samples/apps/iosched/wear/WearApplication.kt
+++ wear/src/main/java/com/google/samples/apps/iosched/wear/WearApplication.kt
@@ -16,14 +16,12 @@
 
 package com.google.samples.apps.iosched.wear
 
-import com.crashlytics.android.Crashlytics
-import com.crashlytics.android.core.CrashlyticsCore
-import com.google.samples.apps.iosched.shared.util.CrashlyticsTree
+
 import com.google.samples.apps.iosched.wear.di.DaggerWearAppComponent
+import com.google.samples.apps.iosched.wear.util.CrashlyticsTree
 import com.jakewharton.threetenabp.AndroidThreeTen
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
-import io.fabric.sdk.android.Fabric
 import timber.log.Timber
 
 /**
@@ -33,15 +31,11 @@ class WearApplication: DaggerApplication () {
     override fun onCreate() {
         super.onCreate()
 
-        // Crashlytics and logging
-        val core = CrashlyticsCore.Builder()
-            .disabled(BuildConfig.DEBUG)
-            .build()
-        Fabric.with(this, Crashlytics.Builder().core(core).build())
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
+        } else {
+            Timber.plant(CrashlyticsTree())
         }
-        Timber.plant(CrashlyticsTree())
 
         // ThreeTenBP for times and dates
         AndroidThreeTen.init(this)
