298aa93edf420cf0e3fad7112d2694c0afc0b628
Doug Sigelbaum <sigelbaum@google.com>
2018-04-23 22:07:06 -0700

    Install LeakCanary on :mobile.
    Bug: 78292889
    Change-Id: I2a75cd1ecbfe243d6af8d14d74e847fb0addfd39

--- mobile/src/main/java/com/google/samples/apps/iosched/MainApplication.kt
+++ mobile/src/main/java/com/google/samples/apps/iosched/MainApplication.kt
@@ -19,6 +19,7 @@ package com.google.samples.apps.iosched
 import com.google.samples.apps.iosched.di.DaggerAppComponent
 import com.google.samples.apps.iosched.util.CrashlyticsTree
 import com.jakewharton.threetenabp.AndroidThreeTen
+import com.squareup.leakcanary.LeakCanary
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 import timber.log.Timber
@@ -29,6 +30,12 @@ import timber.log.Timber
 class MainApplication : DaggerApplication() {
     override fun onCreate() {
         super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         } else {
@@ -37,6 +44,8 @@ class MainApplication : DaggerApplication() {
 
         // ThreeTenBP for times and dates
         AndroidThreeTen.init(this)
+
+        LeakCanary.install(this)
     }
 
     /**
