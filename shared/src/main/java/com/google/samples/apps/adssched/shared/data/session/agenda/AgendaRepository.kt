3aee675f597af9a04cadabd400f490921feac13d
Jonathan Koren <jdkoren@google.com>
2018-10-15 13:30:34 -0700

    New colors for agenda blocks
    Bug: 117782762
    Change-Id: I183542ac5fe4dced33c9c5497ce32da8a2ece968

--- shared/src/main/java/com/google/samples/apps/adssched/shared/data/session/agenda/AgendaRepository.kt
+++ shared/src/main/java/com/google/samples/apps/adssched/shared/data/session/agenda/AgendaRepository.kt
@@ -26,119 +26,142 @@ import javax.inject.Singleton
  */
 @Singleton
 open class AgendaRepository @Inject constructor() {
+
+    companion object {
+        private const val LABEL_REGISTRATION = "Registration"
+        private const val LABEL_KEYNOTE = "Keynote"
+        private const val LABEL_SESSIONS = "Sessions"
+        private const val LABEL_BREAKFAST = "Breakfast"
+        private const val LABEL_LUNCH = "Lunch"
+        private const val LABEL_TEA_BREAK = "Tea Break"
+        private const val LABEL_PARTY = "Party"
+
+        private const val TYPE_REGISTRATION = "badge"
+        private const val TYPE_KEYNOTE = "keynote"
+        private const val TYPE_SESSIONS = "session"
+        private const val TYPE_MEAL = "meal"
+        private const val TYPE_AFTER_HOURS = "after_hours"
+
+        private const val COLOR_REGISTRATION = 0xffe6e6e6.toInt()
+        private const val COLOR_KEYNOTE = 0xfffdc93e.toInt()
+        private const val COLOR_SESSIONS = 0xff73bbf5.toInt()
+        private const val COLOR_MEAL = 0xff9bdd7c.toInt()
+        private const val COLOR_AFTER_HOURS = 0xff202124.toInt()
+    }
+
     private val blocks by lazy {
         listOf(
             Block(
-                title = "Breakfast",
-                type = "meal",
-                color = 0xff31e7b6.toInt(),
+                title = LABEL_BREAKFAST,
+                type = TYPE_MEAL,
+                color = COLOR_MEAL,
                 startTime = ZonedDateTime.parse("2018-11-07T08:00-08:00"),
                 endTime = ZonedDateTime.parse("2018-11-07T10:00-08:00")
             ),
             Block(
-                title = "Registration",
-                type = "badge",
-                color = 0xffe6e6e6.toInt(),
+                title = LABEL_REGISTRATION,
+                type = TYPE_REGISTRATION,
+                color = COLOR_REGISTRATION,
                 startTime = ZonedDateTime.parse("2018-11-07T08:00-08:00"),
                 endTime = ZonedDateTime.parse("2018-11-07T10:00-08:00")
             ),
             Block(
-                title = "Keynote",
-                type = "keynote",
-                color = 0xfffcd230.toInt(),
+                title = LABEL_KEYNOTE,
+                type = TYPE_KEYNOTE,
+                color = COLOR_KEYNOTE,
                 startTime = ZonedDateTime.parse("2018-11-07T10:00-08:00"),
                 endTime = ZonedDateTime.parse("2018-11-07T11:00-08:00")
             ),
             Block(
-                title = "Sessions",
-                type = "session",
-                color = 0xff27e5fd.toInt(),
+                title = LABEL_SESSIONS,
+                type = TYPE_SESSIONS,
+                color = COLOR_SESSIONS,
                 startTime = ZonedDateTime.parse("2018-11-07T11:15-08:00"),
                 endTime = ZonedDateTime.parse("2018-11-07T12:45-08:00")
             ),
             Block(
-                title = "Lunch",
-                type = "meal",
-                color = 0xff31e7b6.toInt(),
+                title = LABEL_LUNCH,
+                type = TYPE_MEAL,
+                color = COLOR_MEAL,
                 startTime = ZonedDateTime.parse("2018-11-07T12:45-08:00"),
                 endTime = ZonedDateTime.parse("2018-11-07T14:00-08:00")
             ),
             Block(
-                title = "Sessions",
-                type = "session",
-                color = 0xff27e5fd.toInt(),
+                title = LABEL_SESSIONS,
+                type = TYPE_SESSIONS,
+                color = COLOR_SESSIONS,
                 startTime = ZonedDateTime.parse("2018-11-07T14:00-08:00"),
                 endTime = ZonedDateTime.parse("2018-11-07T15:30-08:00")
             ),
             Block(
-                title = "Tea Break",
-                type = "meal",
-                color = 0xff31e7b6.toInt(),
+                title = LABEL_TEA_BREAK,
+                type = TYPE_MEAL,
+                color = COLOR_MEAL,
                 startTime = ZonedDateTime.parse("2018-11-07T15:30-08:00"),
                 endTime = ZonedDateTime.parse("2018-11-07T16:00-08:00")
             ),
             Block(
-                title = "Sessions",
-                type = "session",
-                color = 0xff27e5fd.toInt(),
+                title = LABEL_SESSIONS,
+                type = TYPE_SESSIONS,
+                color = COLOR_SESSIONS,
                 startTime = ZonedDateTime.parse("2018-11-07T16:00-08:00"),
                 endTime = ZonedDateTime.parse("2018-11-07T18:20-08:00")
             ),
             Block(
-                title = "Party",
-                type = "after_hours",
-                color = 0xff202124.toInt(),
+                title = LABEL_PARTY,
+                type = TYPE_AFTER_HOURS,
+                color = COLOR_AFTER_HOURS,
                 isDark = true,
                 startTime = ZonedDateTime.parse("2018-11-07T18:20-08:00"),
                 endTime = ZonedDateTime.parse("2018-11-07T22:20-08:00")
             ),
-            
+
             Block(
-                title = "Breakfast",
-                type = "meal",
-                color = 0xff31e7b6.toInt(),
+                title = LABEL_BREAKFAST,
+                type = TYPE_MEAL,
+                color = COLOR_MEAL,
                 startTime = ZonedDateTime.parse("2018-11-08T08:00-08:00"),
                 endTime = ZonedDateTime.parse("2018-11-08T09:30-08:00")
             ),
             Block(
-                title = "Registration",
-                type = "badge",
-                color = 0xffe6e6e6.toInt(),
+                title = LABEL_REGISTRATION,
+                type = TYPE_REGISTRATION,
+                color = COLOR_REGISTRATION,
                 startTime = ZonedDateTime.parse("2018-11-08T08:00-08:00"),
                 endTime = ZonedDateTime.parse("2018-11-08T09:30-08:00")
             ),
             Block(
-                title = "Sessions",
-                type = "session",
-                color = 0xff27e5fd.toInt(),
+                title = LABEL_SESSIONS,
+                type = TYPE_SESSIONS,
+                color = COLOR_SESSIONS,
                 startTime = ZonedDateTime.parse("2018-11-08T09:30-08:00"),
                 endTime = ZonedDateTime.parse("2018-11-08T11:50-08:00")
             ),
             Block(
-                title = "Lunch",
-                type = "meal",
-                color = 0xff31e7b6.toInt(),
+                title = LABEL_LUNCH,
+                type = TYPE_MEAL,
+                color = COLOR_MEAL,
                 startTime = ZonedDateTime.parse("2018-11-08T11:50-08:00"),
                 endTime = ZonedDateTime.parse("2018-11-08T13:05-08:00")
             ),
             Block(
-                title = "Sessions",
-                type = "session",
-                color = 0xff27e5fd.toInt(),
+                title = LABEL_SESSIONS,
+                type = TYPE_SESSIONS,
+                color = COLOR_SESSIONS,
                 startTime = ZonedDateTime.parse("2018-11-08T13:05-08:00"),
                 endTime = ZonedDateTime.parse("2018-11-08T15:25-08:00")
             ),
             Block(
-                title = "Tea Break",
-                type = "meal",
-                color = 0xff31e7b6.toInt(),
+                title = LABEL_TEA_BREAK,
+                type = TYPE_MEAL,
+                color = COLOR_MEAL,
                 startTime = ZonedDateTime.parse("2018-11-08T15:25-08:00"),
                 endTime = ZonedDateTime.parse("2018-11-08T15:55-08:00")
             ),
             Block(
-                title = "Sessions",
-                type = "session",
-                color = 0xff27e5fd.toInt(),
+                title = LABEL_SESSIONS,
+                type = TYPE_SESSIONS,
+                color = COLOR_SESSIONS,
                 startTime = ZonedDateTime.parse("2018-11-08T15:55-08:00"),
                 endTime = ZonedDateTime.parse("2018-11-08T17:25-08:00")
             )
