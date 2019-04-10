8d319f758645341eb1d8b3075804fc93cc3cfdfe
Jonathan Koren <jdkoren@google.com>
2019-04-09 23:01:29 -0700

    Update agenda blocks
    Bug: 127843391
    Change-Id: I52a9d1feb8c3da076c1b76ee3f05e15a6e68da59

--- shared/src/main/java/com/google/samples/apps/iosched/shared/data/agenda/AgendaRepository.kt
+++ shared/src/main/java/com/google/samples/apps/iosched/shared/data/agenda/AgendaRepository.kt
@@ -30,230 +30,224 @@ class DefaultAgendaRepository : AgendaRepository {
     private val blocks by lazy {
         listOf(
             Block(
-                title = "Breakfast",
-                type = "meal",
-                color = 0xff31e7b6.toInt(),
-                startTime = ZonedDateTime.parse("2018-05-08T07:00-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-08T09:30-07:00")
+                title = "Badge pick-up",
+                type = "badge",
+                color = 0xffe6e6e6.toInt(),
+                startTime = ZonedDateTime.parse("2018-05-06T07:00-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-06T19:00-07:00")
             ),
             Block(
                 title = "Badge pick-up",
                 type = "badge",
                 color = 0xffe6e6e6.toInt(),
-                startTime = ZonedDateTime.parse("2018-05-08T07:00-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-08T19:00-07:00")
+                startTime = ZonedDateTime.parse("2018-05-07T07:00-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-07T19:00-07:00")
             ),
             Block(
-                title = "Keynote",
+                title = "Breakfast",
+                type = "meal",
+                color = 0xfffad2ce.toInt(),
+                startTime = ZonedDateTime.parse("2018-05-07T07:00-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-07T09:30-07:00")
+            ),
+            Block(
+                title = "Google Keynote",
                 type = "keynote",
-                color = 0xfffcd230.toInt(),
-                startTime = ZonedDateTime.parse("2018-05-08T10:00-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-08T11:30-07:00")
+                color = 0xfffbbc05.toInt(),
+                startTime = ZonedDateTime.parse("2018-05-07T10:00-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-07T11:30-07:00")
+            ),
+            Block(
+                title = "I/O Store",
+                type = "store",
+                color = 0xffffffff.toInt(),
+                strokeColor = 0xffff6c00.toInt(),
+                startTime = ZonedDateTime.parse("2018-05-07T11:30-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-07T19:30-07:00")
             ),
             Block(
                 title = "Lunch",
                 type = "meal",
-                color = 0xff31e7b6.toInt(),
+                color = 0xfffad2ce.toInt(),
+                startTime = ZonedDateTime.parse("2018-05-07T11:30-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-07T12:45-07:00")
+            ),
+            Block(
+                title = "Developer Keynote",
+                type = "keynote",
+                color = 0xfffbbc05.toInt(),
+                startTime = ZonedDateTime.parse("2018-05-07T12:45-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-07T14:00-07:00")
+            ),
+            Block(
+                title = "Sessions",
+                type = "session",
+                isDark = true,
+                color = 0xff5bb975.toInt(),
+                startTime = ZonedDateTime.parse("2018-05-07T14:00-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-07T19:00-07:00")
+            ),
+            Block(
+                title = "Codelabs",
+                type = "codelab",
+                color = 0xff4285f4.toInt(),
+                isDark = true,
+                startTime = ZonedDateTime.parse("2018-05-07T14:00-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-07T19:00-07:00")
+            ),
+            Block(
+                title = "Office Hours & App Review",
+                type = "office_hours",
+                color = 0xff4285f4.toInt(),
+                isDark = true,
+                startTime = ZonedDateTime.parse("2018-05-07T14:00-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-07T19:00-07:00")
+            ),
+            Block(
+                title = "Sandboxes",
+                type = "sandbox",
+                color = 0xff4285f4.toInt(),
+                isDark = true,
+                startTime = ZonedDateTime.parse("2018-05-07T14:00-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-07T19:30-07:00")
+            ),
+            Block(
+                title = "After Dark",
+                type = "after_hours",
+                color = 0xff164fa5.toInt(),
+                isDark = true,
+                startTime = ZonedDateTime.parse("2018-05-07T18:30-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-07T22:00-07:00")
+            ),
+            Block(
+                title = "Badge & device pick-up",
+                type = "badge",
+                color = 0xffe6e6e6.toInt(),
+                startTime = ZonedDateTime.parse("2018-05-08T08:00-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-08T19:00-07:00")
+            ),
+            Block(
+                title = "Breakfast",
+                type = "meal",
+                color = 0xfffad2ce.toInt(),
+                startTime = ZonedDateTime.parse("2018-05-08T08:00-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-08T10:00-07:00")
+            ),
+            Block(
+                title = "I/O Store",
+                type = "store",
+                color = 0xffffffff.toInt(),
+                strokeColor = 0xffff6c00.toInt(),
+                startTime = ZonedDateTime.parse("2018-05-08T08:00-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-08T20:00-07:00")
+            ),
+            Block(
+                title = "Lunch",
+                type = "meal",
+                color = 0xfffad2ce.toInt(),
                 startTime = ZonedDateTime.parse("2018-05-08T11:30-07:00"),
                 endTime = ZonedDateTime.parse("2018-05-08T14:30-07:00")
             ),
             Block(
-                title = "Codelabs",
-                type = "codelab",
-                color = 0xff4768fd.toInt(),
-                isDark = true,
-                startTime = ZonedDateTime.parse("2018-05-08T11:30-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-08T12:30-07:00")
-            ),
-            Block(
-                title = "Office Hours & App Review",
-                type = "office_hours",
-                color = 0xff4768fd.toInt(),
-                isDark = true,
-                startTime = ZonedDateTime.parse("2018-05-08T11:30-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-08T12:30-07:00")
-            ),
-            Block(
-                title = "I/O Store",
-                type = "store",
-                color = 0xffffffff.toInt(),
-                strokeColor = 0xffff6c00.toInt(),
-                startTime = ZonedDateTime.parse("2018-05-08T11:30-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-08T19:30-07:00")
-            ),
-            Block(
-                title = "Keynote",
-                type = "keynote",
-                color = 0xfffcd230.toInt(),
-                startTime = ZonedDateTime.parse("2018-05-08T12:45-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-08T13:45-07:00")
-            ),
-            Block(
                 title = "Sessions",
                 type = "session",
-                color = 0xff27e5fd.toInt(),
-                startTime = ZonedDateTime.parse("2018-05-08T14:00-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-08T19:00-07:00")
+                isDark = true,
+                color = 0xff5bb975.toInt(),
+                startTime = ZonedDateTime.parse("2018-05-08T08:30-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-08T19:30-07:00")
             ),
             Block(
                 title = "Codelabs",
                 type = "codelab",
-                color = 0xff4768fd.toInt(),
+                color = 0xff4285f4.toInt(),
                 isDark = true,
-                startTime = ZonedDateTime.parse("2018-05-08T14:00-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-08T19:30-07:00")
+                startTime = ZonedDateTime.parse("2018-05-08T08:30-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-08T19:15-07:00")
             ),
             Block(
-                title = "Sandbox",
-                type = "sandbox",
-                color = 0xff4768fd.toInt(),
-                isDark = true,
-                startTime = ZonedDateTime.parse("2018-05-08T14:00-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-08T19:30-07:00")
-            ),
-            Block(
-                title = "Office Hours & App Review",
+                title = "Office Hours & App Reviews",
                 type = "office_hours",
-                color = 0xff4768fd.toInt(),
+                color = 0xff4285f4.toInt(),
                 isDark = true,
-                startTime = ZonedDateTime.parse("2018-05-08T14:00-07:00"),
+                startTime = ZonedDateTime.parse("2018-05-08T08:30-07:00"),
                 endTime = ZonedDateTime.parse("2018-05-08T19:30-07:00")
             ),
             Block(
-                title = "After hours party",
-                type = "after_hours",
-                color = 0xff202124.toInt(),
-                isDark = true,
-                startTime = ZonedDateTime.parse("2018-05-08T19:00-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-08T22:00-07:00")
-            ),
-            Block(
-                title = "Breakfast",
-                type = "meal",
-                color = 0xff31e7b6.toInt(),
-                startTime = ZonedDateTime.parse("2018-05-09T08:00-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-09T10:00-07:00")
-            ),
-            Block(
-                title = "Badge & device pick-up",
-                type = "badge",
-                color = 0xffe6e6e6.toInt(),
-                startTime = ZonedDateTime.parse("2018-05-09T08:00-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-09T19:00-07:00")
-            ),
-            Block(
-                title = "I/O Store",
-                type = "store",
-                color = 0xffffffff.toInt(),
-                strokeColor = 0xffff6c00.toInt(),
-                startTime = ZonedDateTime.parse("2018-05-09T08:00-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-09T20:00-07:00")
-            ),
-            Block(
-                title = "Sessions",
-                type = "session",
-                color = 0xff27e5fd.toInt(),
-                startTime = ZonedDateTime.parse("2018-05-09T08:30-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-09T19:30-07:00")
-            ),
-            Block(
-                title = "Codelabs",
-                type = "codelab",
-                color = 0xff4768fd.toInt(),
-                isDark = true,
-                startTime = ZonedDateTime.parse("2018-05-09T08:30-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-09T20:00-07:00")
-            ),
-            Block(
-                title = "Sandbox",
+                title = "Sandboxes",
                 type = "sandbox",
-                color = 0xff4768fd.toInt(),
+                color = 0xff4285f4.toInt(),
                 isDark = true,
-                startTime = ZonedDateTime.parse("2018-05-09T08:30-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-09T20:00-07:00")
-            ),
-            Block(
-                title = "Office Hours & App Review",
-                type = "office_hours",
-                color = 0xff4768fd.toInt(),
-                isDark = true,
-                startTime = ZonedDateTime.parse("2018-05-09T08:30-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-09T20:00-07:00")
-            ),
-            Block(
-                title = "Lunch",
-                type = "meal",
-                color = 0xff31e7b6.toInt(),
-                startTime = ZonedDateTime.parse("2018-05-09T11:30-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-09T14:30-07:00")
+                startTime = ZonedDateTime.parse("2018-05-08T08:30-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-08T19:15-07:00")
             ),
             Block(
                 title = "Concert",
                 type = "concert",
-                color = 0xff202124.toInt(),
+                color = 0xff164fa5.toInt(),
                 isDark = true,
-                startTime = ZonedDateTime.parse("2018-05-09T19:30-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-09T22:00-07:00")
-            ),
-            Block(
-                title = "Breakfast",
-                type = "meal",
-                color = 0xff31e7b6.toInt(),
-                startTime = ZonedDateTime.parse("2018-05-10T08:00-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-10T10:00-07:00")
+                startTime = ZonedDateTime.parse("2018-05-08T19:45-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-08T22:00-07:00")
             ),
             Block(
                 title = "Badge & device pick-up",
                 type = "badge",
                 color = 0xffe6e6e6.toInt(),
-                startTime = ZonedDateTime.parse("2018-05-10T08:00-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-10T16:00-07:00")
+                startTime = ZonedDateTime.parse("2018-05-09T08:00-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-09T16:00-07:00")
+            ),
+            Block(
+                title = "Breakfast",
+                type = "meal",
+                color = 0xfffad2ce.toInt(),
+                startTime = ZonedDateTime.parse("2018-05-09T08:00-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-09T10:00-07:00")
             ),
             Block(
                 title = "I/O Store",
                 type = "store",
                 color = 0xffffffff.toInt(),
                 strokeColor = 0xffff6c00.toInt(),
-                startTime = ZonedDateTime.parse("2018-05-10T08:00-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-10T17:00-07:00")
-            ),
-            Block(
-                title = "Sessions",
-                type = "session",
-                color = 0xff27e5fd.toInt(),
-                startTime = ZonedDateTime.parse("2018-05-10T08:30-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-10T16:30-07:00")
-            ),
-            Block(
-                title = "Codelabs",
-                type = "codelab",
-                color = 0xff4768fd.toInt(),
-                isDark = true,
-                startTime = ZonedDateTime.parse("2018-05-10T08:30-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-10T16:00-07:00")
-            ),
-            Block(
-                title = "Sandbox",
-                type = "sandbox",
-                color = 0xff4768fd.toInt(),
-                isDark = true,
-                startTime = ZonedDateTime.parse("2018-05-10T08:30-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-10T16:00-07:00")
-            ),
-            Block(
-                title = "Office Hours & App Review",
-                type = "office_hours",
-                color = 0xff4768fd.toInt(),
-                isDark = true,
-                startTime = ZonedDateTime.parse("2018-05-10T08:30-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-10T16:00-07:00")
+                startTime = ZonedDateTime.parse("2018-05-09T08:00-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-09T17:00-07:00")
             ),
             Block(
                 title = "Lunch",
                 type = "meal",
-                color = 0xff31e7b6.toInt(),
-                startTime = ZonedDateTime.parse("2018-05-10T11:30-07:00"),
-                endTime = ZonedDateTime.parse("2018-05-10T14:30-07:00")
+                color = 0xfffad2ce.toInt(),
+                startTime = ZonedDateTime.parse("2018-05-09T11:30-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-09T14:30-07:00")
+            ),
+            Block(
+                title = "Sessions",
+                type = "session",
+                isDark = true,
+                color = 0xff5bb975.toInt(),
+                startTime = ZonedDateTime.parse("2018-05-09T08:30-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-09T16:30-07:00")
+            ),
+            Block(
+                title = "Codelabs",
+                type = "codelab",
+                color = 0xff4285f4.toInt(),
+                isDark = true,
+                startTime = ZonedDateTime.parse("2018-05-09T08:30-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-09T16:00-07:00")
+            ),
+            Block(
+                title = "Office Hours & App Reviews",
+                type = "office_hours",
+                color = 0xff4285f4.toInt(),
+                isDark = true,
+                startTime = ZonedDateTime.parse("2018-05-09T08:30-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-09T16:00-07:00")
+            ),
+            Block(
+                title = "Sandboxes",
+                type = "sandbox",
+                color = 0xff4285f4.toInt(),
+                isDark = true,
+                startTime = ZonedDateTime.parse("2018-05-09T08:30-07:00"),
+                endTime = ZonedDateTime.parse("2018-05-09T16:00-07:00")
             )
         )
     }
