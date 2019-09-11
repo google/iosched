8195c7aa1bec17711217d73eb9c400763096f88f
Jose Alcerreca <jalc@google.com>
2019-09-11 14:36:34 +0200

    Fixes Agenda data in staging
    Change-Id: I681c502ac3da5e96d54fb3ad7b0e5708658fcab8

--- shared/src/staging/java/com/google/samples/apps/iosched/shared/data/FakeAppConfigDataSource.kt
+++ shared/src/staging/java/com/google/samples/apps/iosched/shared/data/FakeAppConfigDataSource.kt
@@ -16,12 +16,87 @@
 
 package com.google.samples.apps.iosched.shared.data
 
+import android.content.res.Resources.NotFoundException
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.google.samples.apps.iosched.shared.data.config.AppConfigDataSource
+import com.google.samples.apps.iosched.shared.data.config.RemoteAppConfigDataSource
+import com.google.samples.apps.iosched.shared.util.TimeUtils
+import org.threeten.bp.ZonedDateTime
+import org.threeten.bp.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME
 
 class FakeAppConfigDataSource : AppConfigDataSource {
+
+    private val times1: Map<String, MutableLiveData<String>> = mapOf(
+        RemoteAppConfigDataSource.REGISTRATION_DAY1_START_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.REGISTRATION_DAY1_END_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.BREAKFAST_DAY1_START_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.BREAKFAST_DAY1_END_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.KEYNOTE_START_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.KEYNOTE_END_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.LUNCH_DAY1_START_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.LUNCH_DAY1_END_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.SESSIONS_DAY1_1_START_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.SESSIONS_DAY1_1_END_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.SESSIONS_DAY1_2_START_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.SESSIONS_DAY1_2_END_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.SESSIONS_DAY1_3_START_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.SESSIONS_DAY1_3_END_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.TEA_BREAK_DAY1_START_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.TEA_BREAK_DAY1_END_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.PARTY_START_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.PARTY_END_TIME to MutableLiveData()
+    )
+
+    private val times2: Map<String, MutableLiveData<String>> = mapOf(
+        RemoteAppConfigDataSource.REGISTRATION_DAY2_START_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.REGISTRATION_DAY2_END_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.BREAKFAST_DAY2_START_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.BREAKFAST_DAY2_END_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.LUNCH_DAY2_START_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.LUNCH_DAY2_END_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.SESSIONS_DAY2_1_START_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.SESSIONS_DAY2_1_END_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.SESSIONS_DAY2_2_START_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.SESSIONS_DAY2_2_END_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.SESSIONS_DAY2_3_START_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.SESSIONS_DAY2_3_END_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.TEA_BREAK_DAY2_START_TIME to MutableLiveData(),
+        RemoteAppConfigDataSource.TEA_BREAK_DAY2_END_TIME to MutableLiveData()
+    )
+
+    private val labels: Map<String, MutableLiveData<String>> = mapOf(
+        RemoteAppConfigDataSource.LABEL_REGISTRATION to MutableLiveData("Registration"),
+        RemoteAppConfigDataSource.LABEL_KEYNOTE to MutableLiveData("Keynote"),
+        RemoteAppConfigDataSource.LABEL_SESSIONS to MutableLiveData("Sessions"),
+        RemoteAppConfigDataSource.LABEL_BREAKFAST to MutableLiveData("Breakfast"),
+        RemoteAppConfigDataSource.LABEL_LUNCH to MutableLiveData("Lunch"),
+        RemoteAppConfigDataSource.LABEL_TEA_BREAK to MutableLiveData("Tea Break"),
+        RemoteAppConfigDataSource.LABEL_PARTY to MutableLiveData("Party")
+    )
+
+    init {
+        val startTimeDay1 = TimeUtils.ConferenceDays[0].start
+        initTimes(startTimeDay1, times1)
+        val startTimeDay2 = TimeUtils.ConferenceDays[1].start
+        initTimes(startTimeDay2, times2)
+    }
+
+    private fun initTimes(
+        startTimeDay2: ZonedDateTime,
+        times: Map<String, MutableLiveData<String>>
+    ) {
+        times.values.forEachIndexed { index, mutableLiveData ->
+            mutableLiveData.postValue(
+                startTimeDay2.plusMinutes(index.toLong()).format(ISO_OFFSET_DATE_TIME)
+            )
+        }
+    }
+
     override suspend fun syncStrings() {}
 
-    override fun getString(key: String): LiveData<String> = MutableLiveData()
+    override fun getString(key: String): LiveData<String> {
+        return times1[key] ?: times2[key] ?: labels[key]
+            ?: throw NotFoundException("Value for $key not found")
+    }
 }
