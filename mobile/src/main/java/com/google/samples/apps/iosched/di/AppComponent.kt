8aa04f886ce84c7775e41ac62f8df6c9c151a591
Jonathan Koren <jdkoren@google.com>
2020-02-28 14:48:37 -0800

    Add filters sheet to SearchFragment
    This adds an empty filters sheet, but does not load any filters or have
    any affect on the search results yet.
    
    Bug: 149929737
    Change-Id: I890a52a9c4fa26e7a48ab10e6a63a84ea67ac570

--- mobile/src/main/java/com/google/samples/apps/iosched/di/AppComponent.kt
+++ mobile/src/main/java/com/google/samples/apps/iosched/di/AppComponent.kt
@@ -22,6 +22,7 @@ import com.google.samples.apps.iosched.shared.di.FeatureFlagsModule
 import com.google.samples.apps.iosched.shared.di.ServiceBindingModule
 import com.google.samples.apps.iosched.shared.di.SharedModule
 import com.google.samples.apps.iosched.shared.di.ViewModelModule
+import com.google.samples.apps.iosched.ui.filters.FiltersViewModelDelegateModule
 import com.google.samples.apps.iosched.ui.signin.SignInViewModelDelegateModule
 import com.google.samples.apps.iosched.ui.theme.ThemedActivityDelegateModule
 import dagger.BindsInstance
@@ -51,7 +52,8 @@ import javax.inject.Singleton
         FeatureFlagsModule::class,
         SignInModule::class,
         SignInViewModelDelegateModule::class,
-        ThemedActivityDelegateModule::class
+        ThemedActivityDelegateModule::class,
+        FiltersViewModelDelegateModule::class
     ]
 )
 interface AppComponent : AndroidInjector<MainApplication> {
