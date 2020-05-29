41ae30e30033bbc5fb42fa3e9f08b15b791703bf
Jose Alcerreca <jalc@google.com>
2020-05-29 14:07:19 +0200

    Fixes flakiness in AgendaTest and cleans up Agenda ViewModel
    Change-Id: I15847372fdca2bbd8eee071568780b99e6f127f4

--- mobile/src/androidTest/java/com/google/samples/apps/iosched/tests/MainTestApplication.kt
+++ mobile/src/androidTest/java/com/google/samples/apps/iosched/tests/MainTestApplication.kt
@@ -18,6 +18,7 @@ package com.google.samples.apps.iosched.tests
 
 import android.app.Application
 import com.jakewharton.threetenabp.AndroidThreeTen
+import timber.log.Timber
 
 /**
  * Used as a base application for Hilt to run instrumented tests through the [CustomTestRunner].
@@ -27,6 +28,7 @@ open class MainTestApplication : Application() {
     override fun onCreate() {
         // ThreeTenBP for times and dates, called before super to be available for objects
         AndroidThreeTen.init(this)
+        Timber.plant(Timber.DebugTree())
         super.onCreate()
     }
 }
