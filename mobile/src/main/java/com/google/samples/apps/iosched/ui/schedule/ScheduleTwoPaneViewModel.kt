4510eecd0ccb41048d7f0d978162a58baa2a2463
Jonathan Koren <jdkoren@google.com>
2021-05-10 13:21:59 -0700

    Toolbar fixes in Schedule
    - Handle up arrow click on Session Detail in single-pane mode
    - Hide the up arrow on Session Detail in two-pane mode
    - Handle up arrow click on Speaker Detail
    - Handle Search app bar programmatically since we care about the width
      of the pane instead of the width of the device.
    
    Bug: 182714988
    Change-Id: I579d9e33681ae9cb573108fe077b8db79914a087

--- mobile/src/main/java/com/google/samples/apps/iosched/ui/schedule/ScheduleTwoPaneViewModel.kt
+++ mobile/src/main/java/com/google/samples/apps/iosched/ui/schedule/ScheduleTwoPaneViewModel.kt
@@ -17,12 +17,32 @@
 package com.google.samples.apps.iosched.ui.schedule
 
 import androidx.lifecycle.ViewModel
+import com.google.samples.apps.iosched.shared.util.tryOffer
 import com.google.samples.apps.iosched.ui.sessioncommon.EventActionsViewModelDelegate
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 import javax.inject.Inject
 
 // Note: clients should obtain this from the Activity.
 @HiltViewModel
 class ScheduleTwoPaneViewModel @Inject constructor(
     eventActionsViewModelDelegate: EventActionsViewModelDelegate
-) : ViewModel(), EventActionsViewModelDelegate by eventActionsViewModelDelegate
+) : ViewModel(), EventActionsViewModelDelegate by eventActionsViewModelDelegate {
+
+    private val _isTwoPane = MutableStateFlow(false)
+    val isTwoPane: StateFlow<Boolean> = _isTwoPane
+
+    private val _returnToListPaneEvents = Channel<Unit>(capacity = Channel.CONFLATED)
+    val returnToListPaneEvents = _returnToListPaneEvents.receiveAsFlow()
+
+    fun setIsTwoPane(isTwoPane: Boolean) {
+        _isTwoPane.value = isTwoPane
+    }
+
+    fun returnToListPane() {
+        _returnToListPaneEvents.tryOffer(Unit)
+    }
+}
