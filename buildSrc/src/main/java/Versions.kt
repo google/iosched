41d471c5d7a81da77c0c953d4a247ef109e0c549
Márton Braun <zsmb@google.com>
2022-03-02 11:01:39 +0100

    Update Room version to 2.4.2

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -19,7 +19,7 @@ object Versions {
     private val versionCodeBase = 70150 // XYYZZM; M = Module (tv, mobile)
     val versionCodeMobile = versionCodeBase + 3
 
-    const val COMPILE_SDK = 30
+    const val COMPILE_SDK = 31
     const val TARGET_SDK = 30
     const val MIN_SDK = 21
 
